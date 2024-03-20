8398406b1061b357f8485f1f6bc1711c36d38cab
Your Name <you@example.com>
2024-03-20 20:26:59 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -41,9 +41,18 @@ debes reservar memoria para cada elemento que agregues.
 Al finalizar retorna la lista creada.
 */
 
-List* crea_lista() {
-   List* L = create_list();
-   return L;
+List* crea_lista() 
+{
+  List* L = create_list();
+
+  for (int i = 1; i < 10; i++)
+    {
+      int *elemento = (int *)malloc(sizeof(int));    
+      *elemento = i;
+      push(L, elemento);
+    }
+
+  return L;
 }
 
 /*
